a1e0a7d4f1855e6a3e77281ea27731017062ad3e
Henrik <31383225+hmkristiansen@users.noreply.github.com>
2020-06-10 22:42:34 +0200

    done some more stuff

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+/*
 window.addEventListener("scroll", function (event) {
     let scroll = this.scrollY,
         windowHeight = this.innerHeight;
@@ -21,3 +23,55 @@ window.addEventListener("scroll", function (event) {
     }
 
 });
+*/
+
+var dataH = [4,5,2,3,4,3,9,9,6,7,8,5,3,2];
+var dataC = [2,1,2,2,1,3,4,7,3,5,6,5,2,1];
+
+var blobH = $('#blobH');
+var blobC = $('#blobC');
+
+
+window.addEventListener("scroll", function (event) {
+    let scroll = this.scrollY,
+        windowHeight = this.innerHeight;
+
+    
+
+});
+
+function translateBlobs(){
+
+
+
+}
+
+
+
+
+
+
+
+
+var idleAni = [".idle1",".idle2"];
+
+window.setInterval(function(){
+    //idleBlobs();
+}, 800);
+
+function idleBlobs(){
+    let hor = getRandomInt(20);
+    let ver = getRandomInt(20);
+    var rot = getRandomInt(180);
+    console.log(hor);
+    console.log(ver);
+
+    $('#blobH svg').css('left',hor);
+    $('#blobH svg').css('top',ver);
+    $('#blobH svg').css('transform','rotate('+rot+'deg)');
+
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
