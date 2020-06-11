bf49f3ef543f8288e7da7e585f7a170cf2b09679
Hinken <henrkri@gmail.com>
2020-06-11 14:17:44 +0200

    scrolling blobs

--- script.js
+++ script.js
@@ -1,77 +1,126 @@
 
-/*
-window.addEventListener("scroll", function (event) {
-    let scroll = this.scrollY,
-        windowHeight = this.innerHeight;
-
-    switch (true){
-        case (scroll < 500):
-            $("#blob_container").removeClass();
-            break;
-        case (scroll > 500 && scroll < 1000):
-            $("#blob_container").removeClass();
-            $('#blob_container').addClass("mode1");
-            break; 
-        case (scroll > 1000 && scroll < 1500):
-            $("#blob_container").removeClass();
-            $('#blob_container').addClass("mode2");
-            break;
-        case (scroll > 1500 && scroll < 2000):
-            $("#blob_container").removeClass();
-            $('#blob_container').addClass("mode3");
-            break;  
-    }
-
-});
-*/
-
-var dataH = [4,5,2,3,4,3,9,9,6,7,8,5,3,2];
-var dataC = [2,1,2,2,1,3,4,7,3,5,6,5,2,1];
+var dataH = [5,5,4,5,2,3,4,3,9,9,6,7,8,5,3,2];
+var dataC = [5,5,2,1,2,2,1,3,4,7,3,5,6,5,2,1];
+var dayDoms = [];
+var triggerPoints = [];
 
 var blobH = $('#blobH');
 var blobC = $('#blobC');
 
+var currentDay;
+var prevMod = 0;
+
+$(document).ready(function(){
+    $(this).scrollTop(0);
+    renderDays();
+    //setTriggerPoints();
+});
+
 
 window.addEventListener("scroll", function (event) {
     let scroll = this.scrollY,
-        windowHeight = this.innerHeight;
+        wh = this.innerHeight;
 
-    
-
+    checkCurrentElement();
+    translateBlobs();
 });
+
+function checkCurrentElement(){
+    let bool = true;
+    let counter = 0;
+    while(bool){
+        var inView = $(dayDoms[counter]).isInViewport();
+        if(inView){
+            bool = false;
+            this.currentDay = counter;
+        }else{
+            counter++;
+        }
+    }
+}
+
+function renderDays(){
+    for(var i=0; i<dataH.length; i++){
+        let dayEvent = document.createElement('div');
+        dayEvent.className = "dayEvent";
+        dayEvent.id = i;
+        $('body').append(dayEvent);
+        this.dayDoms[i]=dayEvent;
+    }
+}
 
 function translateBlobs(){
 
+    console.log("Henrik: "+dataH[currentDay]);
+    console.log("Caro: "+dataC[currentDay]);
 
+    let offset = $('#blobH').width();
+    offset = offset/2;
+
+    let currentDataH;
+    if(dataH[currentDay] < 5){
+        currentDataH = dataH[currentDay] - 5;
+    }else if(dataH[currentDay] == 5){
+        currentDataH = 0;
+    }else{
+        currentDataH = dataH[currentDay];
+    }
+
+    let currentDataC;
+    if(dataC[currentDay] < 5){
+        currentDataC = dataC[currentDay] - 5;
+    }else if(dataC[currentDay] == 5){
+        currentDataC = 0;
+    }else{
+        currentDataC = dataC[currentDay];
+    }
+
+
+    blobH.css('transform','translateX(' + (currentDataH*offset - offset)+ 'px)');
+    blobC.css('transform','translateX(' + (currentDataC*offset- offset)+ 'px)');
 
 }
 
 
+$.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
 
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
 
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+};
 
+/*
+function setTriggerPoints(){
+    for(var i=0; i<dayDoms.length; i++){
+        var scrollTop = $(window).scrollTop(),
+        elementOffset = $(dayDoms[i]).offset().top,
+        distance = (elementOffset - scrollTop);
+        this.triggerPoints[i] = distance;
+    }
+}*/
 
+/*
+// element to detect scroll direction of
+var el = $(window),
 
+    // initialize last scroll position
+    lastY = el.scrollTop();
+    
+el.on('scroll', function() {
+    // get current scroll position
+    var currY = el.scrollTop(),
+        
+        // determine current scroll direction
+        y = (currY > lastY) ? 'down' : ((currY === lastY) ? 'none' : 'up');
 
-var idleAni = [".idle1",".idle2"];
+    // do something here…
+    console.log(y);
 
-window.setInterval(function(){
-    //idleBlobs();
-}, 800);
+    // update last scroll position to current position
+    lastY = currY;
+});
+*/
 
-function idleBlobs(){
-    let hor = getRandomInt(20);
-    let ver = getRandomInt(20);
-    var rot = getRandomInt(180);
-    console.log(hor);
-    console.log(ver);
-
-    $('#blobH svg').css('left',hor);
-    $('#blobH svg').css('top',ver);
-    $('#blobH svg').css('transform','rotate('+rot+'deg)');
-
-}
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-}
